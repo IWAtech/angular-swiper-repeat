78d4b8a5b7d827a1dba1fedee7f639776c4e5583
Paul Weber <paul@iwa-tech.com>
2014-12-15 11:17:02 +0100

    Marking next, previous and current item with classes.

--- swiperRepeat.js
+++ swiperRepeat.js
@@ -184,6 +184,16 @@ var RendererFull = function(slideFactory) {
           }
 
           slide.element.css(this.TRANSFORM_PROP, 'translate(' + i * 100 + '%' + ', 0)');
+
+            var positions = ['previous', 'current', 'next'];
+            for(var index = 0; i < 3; i++) {
+                if(index - 1 == position) {
+                    slide.element.addClass(positions[index]);
+                } else {
+                    slide.element.removeClass(positions[index]);
+                }
+            }
+
         };
       }
     },
@@ -241,6 +251,15 @@ var RendererPart = function(slideFactory) {
         }
 
         slide.element.css(this.TRANSFORM_PROP, 'translate(' + position * 100 + '%' + ', 0)');
+
+        var positions = ['previous', 'current', 'next'];
+        for(var index = 0; i < 3; i++) {
+            if(index - 1 == position) {
+                slide.element.addClass(positions[index]);
+            } else {
+                slide.element.removeClass(positions[index]);
+            }
+        }
       }
     },
 
